8bcc714c778c1bd41201efb892aacd48df5c79cb
masterrshifu <programmingwithakshay@gmail.com>
2023-05-16 14:51:29 +0530

    First Commit

---
+++ ModernJava/src/com/learn/java/lambdas/RunnableLambdaExample.java
@@ -0,0 +1,27 @@
+package com.learn.java.lambdas;
+
+public class RunnableLambdaExample {
+    public static void main(String[] args) {
+        /**
+         * prior Java 8
+         */
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Inside Runnable 1");
+            }
+        };
+        new Thread(runnable).start();
+
+        // java 8 lambda
+
+        // () -> {};
+
+        Runnable runnableLambda = () -> {
+            System.out.println("Inside Runnable 2");
+        };
+
+        new Thread(runnableLambda).start();
+    }
+}
